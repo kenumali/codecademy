e7fed560ec36e53c9d183e92a0b613fb0914ba9f
kenumali <kenneth.n.umali@gmail.com>
2017-01-04 12:05:37 +0000

    Update Continents.java

--- java/Continents.java
+++ java/Continents.java
@@ -1,6 +1,6 @@
-/*
-Practice the Java switch statement.
-This project will print out a continent and the largest city in that continent, based on the value of an integer.
+/** 
+* Practice the Java switch statement.
+* This project will print out a continent and the largest city in that continent, based on the value of an integer.
 */
 
 public class Continents {
