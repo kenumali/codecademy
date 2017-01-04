2e9e1a8ee3d154a5c2513f3f977842bfbd4c5bfb
kenumali <kenneth.n.umali@gmail.com>
2017-01-04 14:19:03 +0000

    Create main.js

---
+++ javascript/litebrite/js/main.js
@@ -0,0 +1,37 @@
+function main(){
+  var colorClass = '';
+  var blinkInterval = '';
+  $('.select-color').on('click', function() {
+    var selectedColor = $(this).attr('class');
+    switch (selectedColor) {
+      case 'select-color cyan not-selected':
+        colorClass = 'cyan';
+        break;
+      case 'select-color yellow not-selected':
+        colorClass = 'yellow';
+        break;
+      case 'select-color magenta not-selected':
+        colorClass = 'magenta';
+        break;
+    }
+  	$(this).removeClass('not-selected');
+    $(this).siblings().addClass('not-selected');
+  });
+  
+  $('.box').on('click', function() {
+    $(this).toggleClass(colorClass);
+  });
+  
+  $('.toggle-blink').on('click', function(){
+    if(colorClass) {
+      $(this).toggleClass('opacity');
+       blinkInterval = setInterval(function() {
+        $('.box.cyan, .box.yellow, .box.magenta').toggleClass('blink');
+      }, 350);
+    }
+    if($('.box.cyan, .box.yellow, .box.magenta').hasClass('blink')) {
+      clearInterval(blinkInterval);
+    }
+  });
+}
+$(document).ready(main);
