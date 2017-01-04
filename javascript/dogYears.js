034e70ebc2e419ca9757e782b2e07bcff8252afc
kenumali <kenneth.n.umali@gmail.com>
2017-01-04 12:18:59 +0000

    Create dogYears.js

---
+++ javascript/dogYears.js
@@ -0,0 +1,9 @@
+/*
+Converts your age into dog years.
+*/
+
+var myAge = prompt('What is your age?');
+var firstYears = 2 * 10.5;
+var laterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+console.log(myAgeInDogYears);
